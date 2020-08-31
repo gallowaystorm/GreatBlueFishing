615cc8bf65f1576df876c1053f99753de0fc3de4
Storm Galloway <storm.galloway@variq.com>
2020-08-31 07:59:46 -0400

    added most components and header for site

--- great-blue-fishing/src/app/app-routing.module.ts
+++ great-blue-fishing/src/app/app-routing.module.ts
@@ -1,8 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './customer/home/home.component';
+import { AboutComponent } from './customer/about/about.component';
+import { StoreComponent } from './customer/store/store.component';
+import { DonationsComponent } from './customer/donations/donations.component';
+import { GalleryComponent } from './customer/gallery/gallery.component';
+import { BlogComponent } from './customer/blog/blog.component';
+import { ContactComponent } from './customer/contact/contact.component';
+import { CreateBlogComponent } from './admin/create-blog/create-blog.component';
+import { ProductsComponent } from './admin/products/products.component';
+import { ManageDonationsComponent } from './admin/manage-donations/manage-donations.component';
+import { ManageUsersComponent } from './admin/manage-users/manage-users.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'store', component: StoreComponent },
+  { path: 'donations', component: DonationsComponent },
+  { path: 'gallery', component: GalleryComponent },
+  { path: 'blog', component: BlogComponent },
+  { path: 'contact', component: ContactComponent},
+  { path: 'admin/blog', component: CreateBlogComponent},
+  { path: 'admin/products', component: ProductsComponent },
+  { path: 'admin/donations', component: ManageDonationsComponent },
+  { path: 'admin/users', component: ManageUsersComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- great-blue-fishing/src/app/app.module.ts
+++ great-blue-fishing/src/app/app.module.ts
@@ -1,18 +1,49 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './customer/home/home.component';
+import { AboutComponent } from './customer/about/about.component';
+import { StoreComponent } from './customer/store/store.component';
+import { DonationsComponent } from './customer/donations/donations.component';
+import { GalleryComponent } from './customer/gallery/gallery.component';
+import { BlogComponent } from './customer/blog/blog.component';
+import { ContactComponent } from './customer/contact/contact.component';
+import { CreateBlogComponent } from './admin/create-blog/create-blog.component';
+import { ManageDonationsComponent } from './admin/manage-donations/manage-donations.component';
+import { ManageUsersComponent } from './admin/manage-users/manage-users.component';
+import { ProductsComponent } from './admin/products/products.component';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    AboutComponent,
+    StoreComponent,
+    DonationsComponent,
+    GalleryComponent,
+    BlogComponent,
+    ContactComponent,
+    CreateBlogComponent,
+    ManageDonationsComponent,
+    ManageUsersComponent,
+    ProductsComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
