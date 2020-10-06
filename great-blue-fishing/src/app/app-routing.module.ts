30c16b3e63da0e48e1f599b4a6fc7bd3e31ddba3
Storm Galloway <storm.galloway@variq.com>
2020-10-06 07:32:50 -0400

    took out redirect on wrong page

--- great-blue-fishing/src/app/app-routing.module.ts
+++ great-blue-fishing/src/app/app-routing.module.ts
@@ -22,7 +22,6 @@ import { OrderCreationStatusComponent } from './customer/checkout/order-creation
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: '**', redirectTo: '', pathMatch: 'full' },
   { path: 'about', component: AboutComponent },
   { path: 'store', component: StoreComponent },
   { path: 'checkout', component: CheckoutComponent, canActivate: [GlobalAuthGuard] },
