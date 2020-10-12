8e67471269e05c185eb8dfa31b86a9222f000749
Storm Galloway <storm.galloway@variq.com>
2020-10-12 13:41:03 -0400

    added customer gallery

--- great-blue-fishing/src/app/customer/gallery/gallery.component.ts
+++ great-blue-fishing/src/app/customer/gallery/gallery.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Gallery } from 'src/app/admin/manage-gallery/gallery.model';
+import { GalleryService } from 'src/app/admin/manage-gallery/gallery.service';
 
 @Component({
   selector: 'app-gallery',
   templateUrl: './gallery.component.html',
   styleUrls: ['./gallery.component.css']
 })
-export class GalleryComponent implements OnInit {
+export class GalleryComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  isLoading = false;
+  private galleryId: string;
+  public galleryImage: Gallery;
+
+  gallery: Gallery[] = [];
+  private gallerySub: Subscription;
+
+  constructor(public galleryService: GalleryService) { }
 
   ngOnInit() {
+    this.getGallery();
+  }
+
+  getGallery(){
+    //for gallery list
+    this.galleryService.getGallery();
+    //gallery posts subscription
+    this.gallerySub = this.galleryService.getGalleryUpdateListener().subscribe((galleryData: { gallery: Gallery[] }) => {
+      this.isLoading = false;
+      this.gallery = galleryData.gallery;
+      });
+  }
+
+  ngOnDestroy(){
+    this.gallerySub.unsubscribe();
   }
 
 }
